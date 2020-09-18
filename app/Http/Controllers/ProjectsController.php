5d7204fd41b0110180f63056d854d863787fb6a4
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-09-18 12:07:04 +0300

    11 - Routing Conventions Worth Following

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Projects;
 use App\Project;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,6 @@ class ProjectsController extends Controller
     public function index()
     {
         $projects = Project::all();
-//        return $projects;
-//        return view('projects.index',['projects' => $projects]);
         return view('projects.index', compact('projects'));
     }
 
@@ -22,12 +21,56 @@ class ProjectsController extends Controller
 
     public function store()
     {
-//        return request()->all();
-//        return request('title');
         $project = new Project();
         $project->title = request('title');
         $project->description = request('description');
         $project->save();
         return redirect('/projects');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Projects $projects)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Projects $projects)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Projects $projects)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Projects $projects)
+    {
+        //
     }
 }
