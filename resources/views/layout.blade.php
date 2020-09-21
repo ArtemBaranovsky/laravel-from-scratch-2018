a427591bfd4e728c08146272f91c0afac537deb3
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-09-21 17:20:25 +0300

    17 - Form Action Considerations

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.0/css/bulma.min.css">
+    <style>
+        .is-complete {
+            text-decoration: line-through;
+        }
+    </style>
     <title>Document1</title>
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,4 @@ Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
 Route::resource('/projects', 'ProjectsController');
-/*Route::get('/projects', 'ProjectsController@index');
-Route::post('/projects', 'ProjectsController@store');
-Route::get('/projects/create', 'ProjectsController@create');
-Route::get('/projects/{project}', 'ProjectsController@show');
-Route::get('/projects/{project}/edit', 'ProjectsController@edit');
-Route::patch('/projects/{project}', 'ProjectsController@update');
-Route::delete('/projects/{project}', 'ProjectsController@destroy');*/
-
+Route::patch('/tasks/{task}', 'ProjectTasksController@update');
